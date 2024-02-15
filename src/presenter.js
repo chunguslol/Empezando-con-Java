f8d680f3a5353be3c5ec1ecc1a8f06d3a3ca742c
Agustin Ledezma <agustin.ledezma@ucb.edu.bo>
2024-02-15 16:15:18 -0400

    segundo guardado

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,12 @@
+// presenter.js
 import sumar from "./sumador";
+import multiplicar from "./multiplicador"; 
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const sumarButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#multiplicar-button"); 
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
@@ -12,4 +16,12 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+// Nuevo evento para el botón de multiplicar
+multiplicarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
